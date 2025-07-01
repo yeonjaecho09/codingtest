14bb1fc78731761e2aa8f527c3847351228b402c
yeonjaecho09 <yeonjaecho@dsm.hs.kr>
2025-07-01 09:01:12 +0900

    Update A×B.cc

--- 백준/Bronze/10998. A×B/A×B.cc
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d", a * b);
-    return 0;
+int a, b;
+scanf("%d %d", &a, &b);
+printf("%d", a * b);
+return 0;
 }
