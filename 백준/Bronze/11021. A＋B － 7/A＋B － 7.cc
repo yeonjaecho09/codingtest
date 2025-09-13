3090f8d9a595bbe99099f07f1481eea668c39948
yeonjaecho09 <yeonjaecho@dsm.hs.kr>
2025-09-13 19:34:59 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() {
+
+	int a, b, n;
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n", i, a + b);
+	}
+}
