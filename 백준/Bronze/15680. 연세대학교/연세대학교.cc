a8ccd77a09bd76c3f233e273c532ea71c9ba0661
yeonjaecho09 <yeonjaecho@dsm.hs.kr>
2025-09-25 18:39:29 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+
+	int n;
+
+	scanf("%d", &n);
+
+	if (n == 0)
+	{
+		printf("YONSEI");
+	}
+	else if (n == 1)
+	{
+		printf("Leading the Way to the Future");
+	}
+}
