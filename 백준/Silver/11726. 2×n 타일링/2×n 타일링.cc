cebf66a3207f2123e7208b321baa263136dbc117
yeonjaecho09 <yeonjaecho@dsm.hs.kr>
2025-12-15 21:44:55 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,11 +1,25 @@
-#include<stdio.h>
- 
-int arr[1001] = { 0,1,2 };
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
 int main() {
-	int n, i;
+	int n = 0;
 	scanf("%d", &n);
-	for (i = 3; i <= n; i++)
+	int* arr;
+	arr = (int*)malloc(sizeof(int) * (n + 1));
+	int sum = 0;
+	if (n >= 1) { arr[1] = 1;}
+	if (n >= 2) { arr[2] = 2;}
+	for (int i = 3; i <= n; i++)
+	{
 		arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
-	printf("%d", arr[n]);
-	return 0;
+		
+	}
+	sum = arr[n];
+	printf("%d", sum);
+
+	free(arr);
+
 }
